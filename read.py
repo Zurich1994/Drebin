4b1f71a80cd761e2ecc75c755c8a9e91dfd24ccc
Zurich1994 <huanran.wang1994@foxmail.com>
2018-10-24 21:23:52 +0800

    Initial commit

---
+++ read.py
@@ -0,0 +1,35 @@
+# coding = utf-8
+import csv
+import os
+import shutil
+
+
+def read_csv():
+    with open('family.csv') as f:
+        f_csv = csv.reader(f, delimiter=',')
+        for row in f_csv:
+            malware_family[row[0]] = row[1]
+
+
+def create_family():
+    with open('family.csv') as f:
+        f_csv = csv.reader(f, delimiter=',')
+        for row in f_csv:
+            family_path = "e:\\Dataset\\Family\\" + row[1]
+            if not os.path.exists(family_path):
+                os.makedirs(family_path)
+
+
+def read_malware():
+    names = os.listdir('Malware')
+    for name in names:
+        fullpath = os.path.join("E:\\Dataset\\Malware", name)
+        dirpath = "E:\\Dataset\\Family\\" + malware_family[name] + "\\" + name + ".apk"
+        shutil.move(fullpath, dirpath)
+
+
+if __name__ == '__main__':
+    malware_family = {}
+    read_csv()
+    create_family()
+    read_malware()
